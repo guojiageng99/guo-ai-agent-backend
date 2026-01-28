a722551c1ff5553a4396a934ce681b5f69d18993
I8I8I8 <448846191@qq.com>
2026-01-28 14:46:31 +0800

    Initial commit

---
+++ src/main/java/com/guo/guoaiagentbackend/GuoAiAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guo.guoaiagentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuoAiAgentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuoAiAgentBackendApplication.class, args);
+    }
+
+}
