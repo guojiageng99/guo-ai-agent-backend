a359218d9614712130aa578c60692a73a3000221
gjg <guojiageng1024@163.com>
2026-02-05 15:40:07 +0800

    mcp server

--- src/test/java/com/guo/guoaiagentbackend/app/LoveAppTest.java
+++ src/test/java/com/guo/guoaiagentbackend/app/LoveAppTest.java
@@ -77,4 +77,12 @@ class LoveAppTest {
         Assertions.assertNotNull(answer);
     }
 
+    @Test
+    void doChatWithMcp() {
+        String chatId = UUID.randomUUID().toString();
+        // 测试地图 MCP
+        String message = "我的另一半居住在上海静安区，请帮我找到 5 公里内合适的约会地点";
+        String answer =  loveApp.doChatWithMcp(message, chatId);
+    }
+
 }
