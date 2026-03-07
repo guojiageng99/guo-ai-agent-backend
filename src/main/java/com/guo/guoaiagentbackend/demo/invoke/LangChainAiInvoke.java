35b5877c379484fa1948b95aee0fc3e760bc9fe1
gjg <guojiageng1024@163.com>
2026-03-07 11:10:27 +0800

    AI大模型接入-Spring AI方式接入还没测试

--- src/main/java/com/guo/guoaiagentbackend/demo/invoke/LangChainAiInvoke.java
+++ src/main/java/com/guo/guoaiagentbackend/demo/invoke/LangChainAiInvoke.java
@@ -1,12 +1,11 @@
 package com.guo.guoaiagentbackend.demo.invoke;
 
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
-import dev.langchain4j.model.chat.ChatLanguageModel;
 
 public class LangChainAiInvoke {
 
     public static void main(String[] args) {
-        ChatLanguageModel qwenModel = QwenChatModel.builder()
+        QwenChatModel qwenModel = QwenChatModel.builder()
                 .apiKey(TestApiKey.API_KEY)
                 .modelName("qwen-max")
                 .build();
