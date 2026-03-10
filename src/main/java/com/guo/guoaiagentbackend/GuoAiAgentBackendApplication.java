4c73ac0d63f6e34f3b1d63df8d4d0f0cd430f5fb
gjg <guojiageng1024@163.com>
2026-03-10 16:53:20 +0800

    PgVector remove project starter object autoconfig

--- src/main/java/com/guo/guoaiagentbackend/GuoAiAgentBackendApplication.java
+++ src/main/java/com/guo/guoaiagentbackend/GuoAiAgentBackendApplication.java
@@ -3,8 +3,12 @@ package com.guo.guoaiagentbackend;
 //import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+        // 为了便于大家开发调试和部署，取消数据库自动配置，需要使用 PgVector 时把 DataSourceAutoConfiguration.class 删除
+        DataSourceAutoConfiguration.class
+})
 public class GuoAiAgentBackendApplication {
 
     public static void main(String[] args) {
