497dfc77f0c406072d5e67393f2b98a291caf088
gjg <guojiageng1024@163.com>
2026-03-10 17:19:10 +0800

    update version finished

--- src/main/java/com/guo/guoaiagentbackend/GuoAiAgentBackendApplication.java
+++ src/main/java/com/guo/guoaiagentbackend/GuoAiAgentBackendApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
+/*@SpringBootApplication(exclude = {
         // 为了便于大家开发调试和部署，取消数据库自动配置，需要使用 PgVector 时把 DataSourceAutoConfiguration.class 删除
         DataSourceAutoConfiguration.class
-})
+})*/
+@SpringBootApplication
 public class GuoAiAgentBackendApplication {
 
     public static void main(String[] args) {
