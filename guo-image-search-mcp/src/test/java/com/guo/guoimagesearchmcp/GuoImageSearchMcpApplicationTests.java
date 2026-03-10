78442c49eb97749a5ebe01b2b1c6238c5ecd8edb
gjg <guojiageng1024@163.com>
2026-03-10 15:25:29 +0800

    MCP protocol first commit

---
+++ guo-image-search-mcp/src/test/java/com/guo/guoimagesearchmcp/GuoImageSearchMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guo.guoimagesearchmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuoImageSearchMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
